c602e86dc11339e21a53cb301e6ca2335437e1a6
SebastianA66 <42424010+SebastianA66@users.noreply.github.com>
2019-03-22 16:05:15 +1100

    Woomy!
    Woomy!

---
+++ Assets/Scripts/Weapon/Seb Weapon/SebWeapon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Seb
+{
+    public class SebWeapon : MonoBehaviour
+    {        
+        public int damage = 34;
+        public float speed = 5f;
+        public int ammo = 6;
+        public float rateOfFire = 5f;
+        public GameObject projectile;
+        public Transform shootPoint;
+        protected int currentAmmo = 0;
+        public bool fire;
+        public Rigidbody rigid;
+        public ParticleSystem particle;
+
+
+        //public abstract void Attack();
+
+        public void Reload()
+        {
+            currentAmmo = ammo;
+        }
+
+        public void Fire(Vector3 direction)
+        {
+            GameObject clone = Instantiate(projectile, shootPoint.transform.position, transform.rotation);
+            rigid.AddForce(direction * speed, ForceMode.Impulse);
+            
+        }
+
+
+    }
+}
