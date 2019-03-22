6a2a474068e9f9037b1d76054297e98f9095a4a5
SebastianA66 <42424010+SebastianA66@users.noreply.github.com>
2019-03-22 13:50:46 +1100

    IDK
    Random

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -13,7 +13,7 @@ public class HealthBar : MonoBehaviour
     {
         healthBar = GetComponent<Slider>();
 
-        healthBar.value = curHealth.value;
+        // healthBar.value = curHealth.value;
     }
 
     // Update is called once per frame
