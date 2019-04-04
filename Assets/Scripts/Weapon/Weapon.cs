38442273bac33dd381ab5984128c9dac3a5ab053
Stephen <42423953+StephenSant@users.noreply.github.com>
2019-04-04 15:43:46 +1100

    FRIGGEN PUSH

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public abstract class Weapon : MonoBehaviour
 {
     public Transform firePoint;
-    public int damage = 8;
+    public int damage;
     [Header("Shooting")]
     public int curAmmo;
-    public int maxAmmo = 20;
-    public float rateOfFire = 2.6f;
-    public float reloadDelay = 1f;
+    public int maxAmmo;
+    public float rateOfFire = 100f;
+    public float reloadDelay = .5f;
     public bool canShoot = true;
 
     protected bool isReloading = false;
 
-    private float shootTimer = 0;
+    public float shootTimer = 0;
 
     public virtual void Start()
     {
@@ -24,19 +24,23 @@ public abstract class Weapon : MonoBehaviour
 
     public virtual void Update()
     {
-        //Debug.DrawRay(muzzle.position, muzzle.forward * range, Color.red);
         shootTimer += Time.deltaTime;
+        Debug.Log(shootTimer+" "+rateOfFire);
         if (shootTimer >= rateOfFire)
         {
             canShoot = true;
         }
-
-        if (curAmmo == 0)
+        else
         {
             canShoot = false;
         }
+        if (curAmmo <= 0)
+        {
+            curAmmo = 0;
+            canShoot = false;
+        }
 
-        UI.instance.ammoText.text = ""+curAmmo;
+        UI.instance.ammoText.text = ""+curAmmo+"/"+maxAmmo;
     }
 
     public virtual void PrimaryFire() { }
